0fd9c3050326b2b31e9b6bb29e1015617a01b3e5
Todd Selwitz <selwitz.todd@gmail.com>
2023-10-17 14:25:52 -0600

    covers more missed test cases

--- acme_insurance/home/tests/__init__.py
+++ acme_insurance/home/tests/__init__.py
@@ -1,6 +1,11 @@
+# View tests
 from home.tests.views.quote_view import QuoteViewTest
+
+# API CRUD view base class test
 from home.tests.utils.api_crud import ApiCrudTest
-from home.tests.models.additional_costs import Additional_Costs
-from home.tests.models.base_coverage import Base_Coverage
-from home.tests.models.quote import Quote
-from home.tests.models.state import State
+
+# models tests
+from home.tests.models.additional_costs import AdditionalCostsModelTest
+from home.tests.models.base_coverage import BaseCoverageModelTest
+from home.tests.models.quote import QuoteModelTest
+from home.tests.models.state import StateModelTest
