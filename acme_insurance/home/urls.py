f09ee99e3e9f879c18b2226bcb4eb9bf2babcf46
Todd Selwitz <selwitz.todd@gmail.com>
2023-10-18 11:58:01 -0600

    routes

--- acme_insurance/home/urls.py
+++ acme_insurance/home/urls.py
@@ -9,24 +9,14 @@ urlpatterns = [
     path(
         "additional_costs/",
         Additional_Costs_View.as_view(),
-        name="additional_costs_list_create",
+        name="additional_costs_list_create_delete",
     ),
     path(
-        "additional_costs/delete/",
-        Additional_Costs_View.as_view(),
-        name="additional_costs_delete",
-    ),
-    path(
-        "base_coverage/", Base_Coverage_View.as_view(), name="base_coverage_list_create"
-    ),
-    path(
-        "base_coverage/delete/",
+        "base_coverage/",
         Base_Coverage_View.as_view(),
-        name="base_coverage_delete",
+        name="base_coverage_list_create_delete",
     ),
-    path("quote/", Quote_View.as_view(), name="quote_list_create"),
+    path("quote/", Quote_View.as_view(), name="quote_list_create_delete"),
     path("quote/rater/", Quote_View.as_view(), name="quote_rater"),
-    path("quote/delete/", Quote_View.as_view(), name="quote_delete"),
     path("state/", State_View.as_view(), name="state_list_create"),
-    path("state/delete/", State_View.as_view(), name="state_delete"),
 ]
